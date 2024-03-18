594b20c358ea4659978ff07fcc50f01a045ceec9
MarkInc3 <mark.k@onther.io>
2024-03-18 10:48:29 +0900

    chore : build file fix

--- univ2/hardhat.config.ts
+++ univ2/hardhat.config.ts
@@ -34,10 +34,56 @@ const config: HardhatUserConfig = {
             }
         },
       },
+      {
+        version: '0.8.24',
+        settings: {
+          evmVersion : 'shanghai' ,
+            optimizer: {
+                enabled: true,
+                runs: 999999,
+            }
+        },
+      },
     ],
       overrides: {
+        "contracts/UniswapV2Rounter2.sol": {
+          version: "0.6.6", 
+          settings: { 
+            optimizer: {
+              enabled: true,
+              runs: 999999,
+            }
+          }
+        },
+        "contracts/libraries/UniswapV2Library.sol": {
+          version: "0.6.6", 
+          settings: { 
+            optimizer: {
+              enabled: true,
+              runs: 999999,
+            }
+          }
+        },
         "contracts/libraries/SafeMath.sol": {
           version: "0.5.16", 
+          settings: { 
+            optimizer: {
+              enabled: true,
+              runs: 999999,
+            }
+          }
+        },
+        "contracts/mocks/MockBTC.sol": {
+          version: "0.8.24", 
+          settings: { 
+            optimizer: {
+              enabled: true,
+              runs: 999999,
+            }
+          }
+        },
+        "contracts/mocks/MockUSDT.sol ": {
+          version: "0.8.24", 
           settings: { 
             optimizer: {
               enabled: true,
@@ -48,11 +94,11 @@ const config: HardhatUserConfig = {
       },
   },
 
-  gasReporter: {
-      enabled: true,
-      currency: 'USD',
-      gasPrice: 21
-  }
+  // gasReporter: {
+  //     enabled: true,
+  //     currency: 'USD',
+  //     gasPrice: 21
+  // }
 };
 
 export default config;
